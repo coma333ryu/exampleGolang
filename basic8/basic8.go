52915893da25208626eec9dc0ac2f36b72b5da11
YoonJeongbu <coma333ryu@gmail.com>
2017-02-17 15:45:47 +0900

    메소드 에제 추가

--- basic8/basic8.go
+++ basic8/basic8.go
@@ -9,6 +9,24 @@ type person struct {
 	age  int
 }
 
+type rect struct {
+	width, height int
+}
+
+//golang용 메서드 value receiver
+//reat가 복사되어 전달된다.
+func (r rect) area() int {
+	r.width = 20
+	return r.width * r.height
+}
+
+//golang용 메서드 포인터 receiver
+//reat의 포인터가 전달된다.
+func (r *rect) area2() int {
+	r.width++
+	return r.width * r.height
+}
+
 //struct는 mutable이고, 다른 함수의 파라메터로 전달시 Pass by Value에 따라 객체를 복사해서 전달하게 된다.
 func changePersonVal(personStruct person) person {
 	personStruct.name = "New Person"
@@ -51,4 +69,13 @@ func main() {
 	fmt.Println("newPerson2", newPerson2)
 	fmt.Println("forthPerson 222====>", forthPerson)
 
+	println("========================= start method =========================")
+	//reat struct가 복사되어 전달되기 때문에 원천 reat의 값에 영향을 주지 않는다.
+	rect := rect{10, 20}
+	area := rect.area()
+	fmt.Println("rect width and area", rect.width, area)
+
+	//reat 포인터가 전달되기 때문에 원천 reat가 변경되어진다.
+	area2 := rect.area2()
+	fmt.Println("rect width and area new ", rect.width, area2)
 }

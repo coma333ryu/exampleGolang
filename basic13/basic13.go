4c17210cd09a77461a32dffb4b2aed5cbadfe154
YoonJeongbu <coma333ryu@gmail.com>
2017-02-22 15:41:26 +0900

    채널 기본예제

---
+++ basic13/basic13.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func pinger(c chan string) {
+	for i := 0; i < 10; i++ {
+		//<-를 이용하여 채널에 값 전달
+		c <- "ping"
+	}
+}
+func printer(c chan string) {
+	for {
+		//<-를 이용하여 채널의 값을 가져옴
+		msg := <-c
+		fmt.Println(msg)
+		time.Sleep(time.Second * 1)
+	}
+}
+func main() {
+	//make를 이용하여 channel 생성
+	c := make(chan string)
+
+	//함수 pinger와 printer에 channel전달
+	go pinger(c)
+	go printer(c)
+
+	//main함수도 goroutine이기 때문에 Scanln를 사용하여 goroutine이 종료되지 않게 막음.
+	var input string
+	fmt.Scanln(&input)
+
+	/*
+		done := make(chan bool)
+		go func() {
+			for i := 0; i < 10; i++ {
+				fmt.Println(i)
+			}
+			done <- true
+		}()
+
+		// 위의 Go루틴이 끝날 때까지 대기
+		<-done
+	*/
+}
